d95234849cb20b27051323fccf008198b860d8aa
hoge <hosoki.r.aa@m.titech.ac.jp>
2021-07-03 12:13:12 +0900

    21/7/3

--- contest/reset.sh
+++ contest/reset.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-cp ../Format/formatCpp.cpp a.cpp
-cp ../Format/formatCpp.cpp b.cpp
-cp ../Format/formatCpp.cpp c.cpp
-cp ../Format/formatCpp.cpp d.cpp
-cp ../Format/formatCpp.cpp e.cpp
-cp ../Format/formatCpp.cpp f.cpp
+cp ../template/template.cpp a.cpp
+cp ../template/template.cpp b.cpp
+cp ../template/template.cpp c.cpp
+cp ../template/template.cpp d.cpp
+cp ../template/template.cpp e.cpp
+cp ../template/template.cpp f.cpp
+rm *.out
--- library/point.cpp
+++ library/point.cpp
@@ -38,5 +38,5 @@ struct Point {
 };
 
 bool onLine(Point p1, Point p2, Point p3) {
-  return (p2.x-p1.x)*(p3.y-p1.y) == (p2.y-p1.y)*(p3.x-p1.x)
+  return (p2.x-p1.x)*(p3.y-p1.y) == (p2.y-p1.y)*(p3.x-p1.x);
 }
