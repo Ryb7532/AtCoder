8fdfef4c5f38109d4d54a6e343eebbd437975688
hoge <hosoki.r.aa@m.titech.ac.jp>
2021-07-10 18:36:20 +0900

    21/07/10

--- library/point.cpp
+++ library/point.cpp
@@ -1,30 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
+typedef long long T;
 typedef long double ld;
 
-
+template <typename T>
 struct Point {
-  ll x; ll y;
-  Point(ll a=0,ll b=0): x(a), y(b) {}
+  T x; T y;
+  Point(T a=0,T b=0): x(a), y(b) {}
   Point &operator+=(const Point &p) { x+=p.x; y+=p.y; return *this; }
-  Point &operator+=(const ll c) { x+=c; y+=c; return *this; }
+  Point &operator+=(const T c) { x+=c; y+=c; return *this; }
   Point &operator-=(const Point &p) { x-=p.x; y-=p.y; return *this; }
-  Point &operator-=(const ll c) { x-=c; y-=c; return *this; }
+  Point &operator-=(const T c) { x-=c; y-=c; return *this; }
   Point &operator*=(const Point &p) { x*=p.x; y*=p.y; return *this; }
-  Point &operator*=(const ll &c) { x*=c; y*=c; return *this; }
+  Point &operator*=(const T &c) { x*=c; y*=c; return *this; }
   Point operator-() const{ return Point(-x,-y); }
   Point operator+(const Point &p) const { return Point(*this)+=p; }
   Point operator-(const Point &p) const { return Point(*this)-=p; }
   Point operator*(const Point &p) const { return Point(*this)*=p; }
-  friend Point operator*(const Point &p, const ll c) { return Point(p) *= c; }
-  friend Point operator*(const ll c, const Point &p) { return Point(p) *= c; }
+  friend Point operator*(const Point &p, const T c) { return Point(p) *= c; }
+  friend Point operator*(const T c, const Point &p) { return Point(p) *= c; }
   bool operator==(const Point &p) const { return x==p.x && y==p.y; }
   bool operator!=(const Point &p) const { return x!=p.x || y!=p.y; }
   Point swap() const { return Point(y,x); }
   friend ostream &operator<<(ostream &os, const Point &p) { return os << p.x << "," << p.y; }
   friend istream &operator>>(istream &is, Point &p) {
-    ll a,b;
+    T a,b;
     is >> a >> b;
     p = Point(a,b);
     return (is);
@@ -37,6 +37,7 @@ struct Point {
   }
 };
 
-bool onLine(Point p1, Point p2, Point p3) {
+template <typename T>
+bool onLine(Point<T> p1, Point<T> p2, Point<T> p3) {
   return (p2.x-p1.x)*(p3.y-p1.y) == (p2.y-p1.y)*(p3.x-p1.x);
 }
