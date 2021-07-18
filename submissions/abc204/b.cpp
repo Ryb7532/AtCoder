1f9bb8248c837b49b9876282d922da07091dc666
hoge <hosoki.r.aa@m.titech.ac.jp>
2021-07-18 15:22:53 +0900

    21/07/18

---
+++ submissions/abc204/b.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define vec vector
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define print(a) cout << a << endl
+#define fix(n) fixed << setprecision(n)
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define Yes "Yes"
+#define YES "YES"
+#define No "No"
+#define NO "NO"
+
+
+int main() {
+  int N;
+  cin >> N;
+  int A, res = 0;
+  rep(i,N) {
+    cin >> A;
+    res += max(A-10,0);
+  }
+  print(res);
+  return 0;
+}
