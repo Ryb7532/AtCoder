30cea91564074e3c3b48187a6e3a0e7eb1882799
hoge <hosoki.r.aa@m.titech.ac.jp>
2021-10-23 15:49:40 +0900

    21/10/23

---
+++ submissions/abc215/b.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define vec vector
+#define rep(i,n) for (int i=0; i<(n); i++)
+#define print(a) cout << a << endl
+#define fix(n) fixed << setprecision(n)
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define Yes "Yes"
+#define YES "YES"
+#define No "No"
+#define NO "NO"
+
+
+int main() {
+  ll N;
+  cin >> N;
+  int k=0;
+  for (; k<60; k++) {
+    if (1LL << k > N)
+      break;
+  }
+  print(k-1);
+  return 0;
+}
