c764d945a083db8cfbede7ac3625b1f8fa80ac6a
hoge <Ryb7532@user.noreply.github.com>
2021-12-22 23:05:34 +0900

    21/12/22

--- template/template.cpp
+++ template/template.cpp
@@ -6,7 +6,7 @@ typedef long double ld;
 #define rep(i,n) for (int i=0; i<(n); i++)
 #define print(a) cout << a << endl
 #define fix(n) fixed << setprecision(n)
-#define fill(c, n) setfill(c) << setw(n)
+#define fill_c(c, n) setfill(c) << setw(n)
 #define all(v) v.begin(), v.end()
 #define rall(v) v.rbegin(), v.rend()
 #define Yes "Yes"
