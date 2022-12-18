1bcebc878022a5ce7edf87f47bcb21e4ea8aec5c
hoge <Ryb7532@user.noreply.github.com>
2022-12-18 23:41:05 +0900

    add submit function

--- project.sh
+++ project.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-read project
-acc new $project
-cp template/test.sh $project/test.sh
-cp template/template.cpp $project/a/a.cpp
-cp template/template.cpp $project/b/b.cpp
-cp template/template.cpp $project/c/c.cpp
-cp template/template.cpp $project/d/d.cpp
-cp template/template.cpp $project/e/e.cpp
-cp template/template.cpp $project/f/f.cpp
-cp template/template.cpp $project/g/g.cpp
-cp template/template.cpp $project/h/h.cpp
-chmod u+x $project/test.sh
-cd $project
+printf "Which contest?\n"
+read contest
+acc login
+oj login https://beta.atcoder.jp/
+acc new $contest
+cp template/test.sh $contest/test.sh
+chmod u+x $contest/test.sh
+cd $contest
--- template/test.sh
+++ template/test.sh
@@ -1,21 +1,42 @@
 #!/bin/bash
 
+FLAG_SUBMIT=1
+while (( $# > 0 ))
+do
+  case $1 in
+    -n)
+      FLAG_SUBMIT=0
+      ;;
+    -*)
+      echo "invalid option"
+      exit 1
+      ;;
+  esac
+  shift
+done
+
 printf "which problem?\n"
 read problem
 cd $problem
-g++ -Wall -o $problem $problem.cpp
+g++ -Wall -o a.out main.cpp
+export TIMEFORMAT="%Es"
 i=1
 while [ -s tests/sample-$i.in ];
 do
-  ./$problem < tests/sample-$i.in > tests/answer-$i.out
+  runtime=`(time ./a.out < tests/sample-$i.in > tests/answer-$i.out)2>&1`
   diff tests/sample-$i.out tests/answer-$i.out > tests/text.txt
   if [ -s tests/text.txt ]; then
-    printf "\e[31merror\e[m\n"
+    printf "\e[31merror\e[m\t\t${runtime}\n"
+    FLAG_SUBMIT=0
   else
-    printf "\e[32msuccess\e[m\n"
+    printf "\e[32msuccess\e[m\t\t${runtime}\n"
     rm tests/answer-$i.out
   fi
 
   rm tests/text.txt
   i=`expr $i + 1 `
 done
+
+if [ $FLAG_SUBMIT == 1 ]; then
+  echo abc${problem} | acc submit
+fi
