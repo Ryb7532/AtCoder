66e14b9165a4482aef2426dcb4fca36b5a219aa8
hoge <Ryb7532@user.noreply.github.com>
2023-03-24 21:56:03 +0900

    revise scraping.py

--- submissions/scraping.py
+++ submissions/scraping.py
@@ -56,6 +56,10 @@ import subprocess
 import geckodriver_autoinstaller
 
 geckodriver_autoinstaller.install()
+# FireFoxが更新された直後ではメッセージによってスクレイピングができなくなるため，一度Firefoxを立ち上げ終了する
+driver = webdriver.Firefox()
+driver.quit()
+
 driver = webdriver.Firefox()
 
 add_cnt = 0
