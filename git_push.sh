016730822e62fb7de16991d6e0bafd12683a28a1
hoge <Ryb7532@user.noreply.github.com>
2023-04-19 20:26:26 +0900

    revise the bug that cannot do scraping when Firefox was updated.

--- git_push.sh
+++ git_push.sh
@@ -5,4 +5,8 @@ python3 scraping.py
 git add .
 today=`date "+%y/%m/%d"`
 git commit -m "${today}"
-git push
+echo "Will you push? (if not, type n.)"
+read push
+if [ $push != 'n' ]; then
+  git push
+fi
