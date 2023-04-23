0c13543fc4b28210133e13301a4e8a48a6628019
hoge <Ryb7532@user.noreply.github.com>
2023-04-23 13:09:06 +0900

    revise matrix library to be able to deal with a rig (semi-ring)

--- library/matrix.cpp
+++ library/matrix.cpp
@@ -1,44 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define rep(i,n) for (int i=0; i<(int)(n); i++)
+
+
+const ll aid = 0; // Additive identity
+const ll mid = 1; // Multiplicative identity
 
 // Matrix
 template< typename T >
 struct Matrix {
   vector<vector<T>> A;
-  Matrix(size_t n, size_t m): A(n,vector<T>(m,0)) {};
-  Matrix(size_t n): A(n,vector<T>(n,0)) {};
+  Matrix(size_t n, size_t m): A(n,vector<T>(m,aid)) {};
+  Matrix(size_t n): A(n,vector<T>(n,aid)) {};
   size_t height() const { return (A.size()); }
   size_t width() const { return (A[0].size()); }
   inline const vector< T > &operator[](int k) const { return A.at(k); }
   inline vector< T > &operator[](int k) { return A.at(k); }
   static Matrix I(size_t n) {
     Matrix mat(n);
-    for(int i=0; i<n; i++) mat[i][i] = 1;
+    rep(i,n) mat[i][i] = mid;
     return mat;
   }
   Matrix &operator+=(const Matrix &B) {
     size_t n = height(), m = width();
     assert(n == B.height() && m == B.width());
-    for(int i=0; i<n; i++)
-      for(int j=0; j<m; j++)
+    rep(i,n)
+      rep(j,m)
         (*this)[i][j] += B[i][j];
     return *this;
   }
   Matrix &operator-=(const Matrix &B) {
     size_t n = height(), m = width();
     assert(n == B.height() && m == B.width());
-    for(int i=0; i<n; i++)
-      for(int j=0; j<m; j++)
+    rep(i,n)
+      rep(j,m)
         (*this)[i][j] -= B[i][j];
     return *this;
   }
   Matrix &operator*=(const Matrix &B) {
     size_t n = height(), m = B.width(), p = width();
     assert(p == B.height());
-    vector<vector< T >> C(n, vector< T >(m,0));
-    for(int i=0; i<n; i++)
-      for(int j=0; j<m; j++)
-        for(int k=0; k<p; k++)
+    vector<vector<T>> C(n, vector<T>(m,aid));
+    rep(i,n)
+      rep(j,m)
+        rep(k,p)
           C[i][j] = (C[i][j] + (*this)[i][k] * B[k][j]);
     A.swap(C);
     return *this;
@@ -59,9 +64,9 @@ struct Matrix {
   Matrix operator^(const long long k) const { return Matrix(*this) ^= k; }
   friend ostream &operator<<(ostream &os, Matrix &p) {
     size_t n = p.height(), m = p.width();
-    for(int i=0; i<n; i++) {
+    rep(i,n) {
       os << "[";
-      for(int j=0; j<m; j++)
+      rep(j,m)
         os << p[i][j] << (j+1 == m ? "]\n" : ",");
     }
     return os;
@@ -70,24 +75,24 @@ struct Matrix {
     Matrix B(*this);
     assert(width() == height());
     T ret = 1;
-    for(int i=0; i<width(); i++) {
+    rep(i,height()) {
       int idx = -1;
-      for(int j=i; j<width(); j++) {
-        if(B[j][i]!=0) idx = j;
+      rep(j,height()) {
+        if(B[j][i]!=aid) idx = j;
       }
-      if(idx==-1) return (0);
+      if(idx==-1) return (aid);
       if(i!=idx) {
         ret *= -1;
         swap(B[i], B[idx]);
       }
       ret *= B[i][i];
       T vv = B[i][i];
-      for(int j=0; j<width(); j++) {
+      rep(j,height()) {
         B[i][j] /= vv;
       }
-      for(int j=i+1; j<width(); j++) {
+      for(int j=i+1; j<(int)width(); j++) {
         T a = B[j][i];
-        for(int k=0; k<width(); k++) {
+        rep(k,height()) {
           B[j][k] -= B[i][k] * a;
         }
       }
