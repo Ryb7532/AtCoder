de6418eb1f5c4c0f6d8965cce0e1509dcf1bee9d
hoge <Ryb7532@user.noreply.github.com>
2023-04-25 19:14:52 +0900

    add an option not to always get all tasks

--- project.sh
+++ project.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
+OPT_CONFIG=""
+while (( $# > 0 ))
+do
+  case $1 in
+    -c)
+      OPT_CONFIG="-c inquire"
+      ;;
+    -*)
+      ;;
+  esac
+  shift
+done
+
 printf "Which contest?\n"
 read contest
 acc login
 oj login https://beta.atcoder.jp/
-acc new $contest
+acc new $contest $OPT_CONFIG
 cp template/test.sh $contest/.
 cp template/check_testcase.out $contest/.
 chmod u+x $contest/test.sh
