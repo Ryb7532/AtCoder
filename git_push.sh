b2f1feb7be70baa19efb831cf3625c829f442252
Ryb7532 <ryb09137532@gmail.com>
2023-07-17 10:41:51 +0200

    revise commit name creation

--- git_push.sh
+++ git_push.sh
@@ -3,7 +3,7 @@
 cd submissions
 python3 scraping.py
 git add .
-today=`date "+%y/%m/%d"`
+today=`date "+%y/%m/%d %H:%M"`
 git commit -m "${today}"
 echo "Will you push? (if not, type n.)"
 read flag_push
