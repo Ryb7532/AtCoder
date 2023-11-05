c9159ad3f7fe67ebf7dce5253b6f0509e17268ba
Ryb7532 <ryb09137532@gmail.com>
2023-11-05 13:26:21 +0900

    change funcs to 1-line

--- library/maze.cpp
+++ library/maze.cpp
@@ -6,10 +6,6 @@ int H, W;
 vector<int> dx = {-1,0,1,0}, dy = {0,-1,0,1};
 // vector<int> dx = {-1,-1,-1,0,1,1,1,0}, dy = {1,0,-1,-1,-1,0,1,1};
 
-pair<int,int> newPos(int x, int y, int id, int step=1) {
-  return {x+step*dx[id], y+step*dy[id]};
-}
+pair<int,int> newPos(int x, int y, int id, int step=1) {return {x+step*dx[id], y+step*dy[id]};}
 
-bool isRange(int h, int w) {
-  return h>=0 && h<H && w>=0 && w<W;
-}
+bool isRange(int h, int w) {return h>=0 && h<H && w>=0 && w<W;}
