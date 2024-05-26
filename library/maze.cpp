64cc1599a4a0f497477a00aac83d76a9b01508f7
Ryb7532 <ryb09137532@gmail.com>
2024-05-26 18:05:36 +0900

    revise grid library and template

--- library/maze.cpp
+++ library/maze.cpp
@@ -2,33 +2,31 @@
 using namespace std;
 #define rep(i, n) for (int i = 0; i < (n); i++)
 
-
-int H, W;
-int dx4[] = {-1,0,1,0}, dy4[] = {0,-1,0,1};
-int dx8[] = {-1,-1,-1,0,1,1,1,0}, dy8[] = {1,0,-1,-1,-1,0,1,1};
+int H,W;
 struct Grid {
   int x;
   int y;
+
   Grid(int x=0, int y=0) : x(x), y(y) {}
 
   bool isRange() { return 0<=x && x<H && 0<=y && y<W; }
 
-  vector<Grid> newPos(int step=1) {
+  vector<Grid> newPos(int step=1, bool dia=false) {
     vector<Grid> res;
-    rep(i,4) {
-      res.emplace_back(Grid(x+step*dx4[i], y+step*dy4[i]));
+    res.emplace_back(Grid(x+step, y));
+    res.emplace_back(Grid(x, y+step));
+    res.emplace_back(Grid(x-step, y));
+    res.emplace_back(Grid(x, y-step));
+    if (dia) {
+      res.emplace_back(Grid(x+step, y+step));
+      res.emplace_back(Grid(x-step, y+step));
+      res.emplace_back(Grid(x-step, y-step));
+      res.emplace_back(Grid(x+step, y-step));
     }
     return res;
   }
 
-  vector<Grid> newPos_8(int step=1) {
-    vector<Grid> res;
-    rep(i,8) {
-      res.emplace_back(Grid(x+step*dx8[i], y+step*dy8[i]));
-    }
-    return res;
-  }
-
+  Grid &operator++() { y++; if (y >= W) x++, y=0; return *this; }
   bool operator==(const Grid &g) const { return x==g.x && y==g.y; }
   bool operator!=(const Grid &g) const { return x!=g.x || y!=g.y; }
   friend ostream &operator<<(ostream &os, const Grid &g) { return os << g.x << " " << g.y; }
--- template/cpp/main.cpp
+++ template/cpp/main.cpp
@@ -5,21 +5,26 @@ typedef long double ld;
 #define GET_MACRO(_1,_2,_3,NAME,...) NAME
 #define rep3(i,s,e) for (int i=(int)s; i<(int)e; i++)
 #define rep2(i,n) rep3(i,0,n)
-#define rep(...) GET_MACRO(__VA_ARGS__,rep3,rep2)(__VA_ARGS__)
+#define rep1(n) rep2(_,n)
+#define rep(...) GET_MACRO(__VA_ARGS__,rep3,rep2,rep1)(__VA_ARGS__)
 #define rrep3(i,s,e) for (int i=(int)e-1; i>=(int)s; i--)
 #define rrep2(i,n) rrep3(i,0,n)
-#define rrep(...) GET_MACRO(__VA_ARGS__,rrep3,rrep2)(__VA_ARGS__)
+#define rrep1(n) rrep2(_,n)
+#define rrep(...) GET_MACRO(__VA_ARGS__,rrep3,rrep2,rrep1)(__VA_ARGS__)
 #define fore(e,v) for (auto &e: v)
 #define all(v) v.begin(), v.end()
 #define rall(v) v.rbegin(), v.rend()
 #define chmin(a,b) a = min(a,b)
 #define chmax(a,b) a = max(a,b)
-#define print(a) cout << a << endl
+#define out1(a) cout << (a) << endl
+#define out2(a,b) cout << (a) << " " << (b) << endl
+#define out3(a,b,c) cout << (a) << " " << (b) << " " << (c) << endl
+#define print(...) GET_MACRO(__VA_ARGS__,out3,out2,out1)(__VA_ARGS__)
 #define fix(n) fixed << setprecision(n)
 #define fill_c(c, n) setfill(c) << setw(n)
 #define Yes "Yes"
 #define No "No"
-#define printyesno(f) cout << (f ? Yes : No) << endl
+#define printyn(f) cout << (f ? Yes : No) << endl
 #define printall(a) for (auto e: a) cout << e << " "; cout << endl
 
 int main() {
