9a4a020abcc70a1a438dfc5067d9d661532dacfe
Ryb7532 <ryb09137532@gmail.com>
2024-07-14 10:00:52 +0900

    revise struct Grid

--- library/maze.cpp
+++ library/maze.cpp
@@ -3,37 +3,48 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < (n); i++)
 
 int H,W;
-struct Grid {
-  int x;
-  int y;
+struct Grid: pair<int,int> {
+  inline int &x() { return first; }
+  inline int &y() { return second; }
+  Grid(int x=0, int y=0) { first=x, second=y; }
+  Grid(const pair<int,int> p) { first=p.first, second=p.second; }
 
-  Grid(int x=0, int y=0) : x(x), y(y) {}
-
-  bool isRange() { return 0<=x && x<H && 0<=y && y<W; }
-
-  vector<Grid> newPos(int step=1, bool dia=false) {
-    vector<Grid> res;
-    res.emplace_back(Grid(x+step, y));
-    res.emplace_back(Grid(x, y+step));
-    res.emplace_back(Grid(x-step, y));
-    res.emplace_back(Grid(x, y-step));
-    if (dia) {
-      res.emplace_back(Grid(x+step, y+step));
-      res.emplace_back(Grid(x-step, y+step));
-      res.emplace_back(Grid(x-step, y-step));
-      res.emplace_back(Grid(x+step, y-step));
-    }
-    return res;
+  Grid &operator+=(const pair<int,int> &p) {
+    x() += p.first;
+    y() += p.second;
+    return *this;
   }
-
-  Grid &operator++() { y++; if (y >= W) x++, y=0; return *this; }
-  bool operator==(const Grid &g) const { return x==g.x && y==g.y; }
-  bool operator!=(const Grid &g) const { return x!=g.x || y!=g.y; }
-  friend ostream &operator<<(ostream &os, const Grid &g) { return os << g.x << " " << g.y; }
+  Grid &operator-=(const pair<int,int> &p) {
+    x() -= p.first;
+    y() -= p.second;
+    return *this;
+  }
+  Grid operator+(const pair<int,int> &p) const { return Grid(*this) += p; }
+  Grid operator-(const pair<int,int> &p) const { return Grid(*this) -= p; }
+  Grid &operator++() { x()++; if (y() >= W) x()++, y()=0; return *this; }
+  friend ostream &operator<<(ostream &os, const Grid &g) { return os << g.first << " " << g.second; }
   friend istream &operator>>(istream &is, Grid &g) {
     int a,b;
     is >> a >> b;
+    a--; b--;
     g = Grid(a,b);
     return (is);
+  }
+
+  bool isRange() { return 0<=x() && x()<H && 0<=y() && y()<W; }
+
+  vector<Grid> newPos(int step=1, bool dia=false) {
+    vector<Grid> res;
+    res.emplace_back(Grid(x()+step, y()));
+    res.emplace_back(Grid(x(),      y()+step));
+    res.emplace_back(Grid(x()-step, y()));
+    res.emplace_back(Grid(x(),      y()-step));
+    if (dia) {
+      res.emplace_back(Grid(x()+step, y()+step));
+      res.emplace_back(Grid(x()-step, y()+step));
+      res.emplace_back(Grid(x()-step, y()-step));
+      res.emplace_back(Grid(x()+step, y()-step));
+    }
+    return res;
   }
 };
