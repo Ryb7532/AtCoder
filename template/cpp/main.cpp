8e336c3e742aa17f5511a24d950ae7e7129a3ef4
Ryb7532 <ryb09137532@gmail.com>
2024-07-18 02:02:28 +0900

    revise print func

--- template/cpp/main.cpp
+++ template/cpp/main.cpp
@@ -11,20 +11,17 @@ typedef long double ld;
 #define rrep2(i,n) rrep3(i,0,n)
 #define rrep(...) GET_MACRO(__VA_ARGS__,rrep3,rrep2)(__VA_ARGS__)
 #define fore(e,v) for (auto &e: v)
-#define all(v) v.begin(), v.end()
-#define rall(v) v.rbegin(), v.rend()
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
 #define chmin(a,b) a = min(a,b)
 #define chmax(a,b) a = max(a,b)
-#define out1(a) cout << (a) << endl
-#define out2(a,b) cout << (a) << " " << (b) << endl
-#define out3(a,b,c) cout << (a) << " " << (b) << " " << (c) << endl
-#define print(...) GET_MACRO(__VA_ARGS__,out3,out2,out1)(__VA_ARGS__)
 #define fix(n) fixed << setprecision(n)
 #define fill_c(c, n) setfill(c) << setw(n)
 #define Yes "Yes"
 #define No "No"
-#define printyn(f) cout << (f ? Yes : No) << endl
+#define printyn(f) cout << ((f) ? Yes : No) << endl
 #define printall(a) {for (auto e: a) cout << e << " "; cout << endl;}
+template<typename... Args> void print(const Args... args) {(cout << ... << (cout << ' ', args)) << endl;}
 
 int main() {
   
